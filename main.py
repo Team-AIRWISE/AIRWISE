cfc07fb667e2aff9b0650295b2a2f2dc29667302
s12346938 <113122133+s12346938@users.noreply.github.com>
2024-01-31 13:42:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from time import sleep
 from KitronikAirQualityControlHAT import *
+bme688 = KitronikBME688()
 oled = KitronikOLED()
 hpo1 = KitronikHighPowerOut(1)
 hpo2 = KitronikHighPowerOut(2)
@@ -10,11 +11,19 @@ oled.show()
 sleep(5)
 oled.clear()
 
-bme688 = KitronikBME688()
+bme688.measureData()
 humidity=bme688.readHumidity()
+c02=bme688.readeCO2()
+bme688.getAirQualityPercent()
+
 if humdity<43:
-  hpo1 = KitronikHighPowerOut(1)
+  hpo1.turnOn()
 elif humidity>47:
-  hpo2 = KitronikHighPowerOut(2)
+  hpo2.turnOn()
 else:
+  KitronikHighPowerOut(1)
+  hpo1.turnOff()
+  hpo2.turnOff()
+
+if c02>800:
   
