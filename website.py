8a4e94e78b814e867d791c0ba3eb268406cd5634
s12346938 <113122133+s12346938@users.noreply.github.com>
2024-02-28 18:58:27 +0000

    WEB

--- website.py
+++ website.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('/home/pi/AIRWISE/airwise.html')
+    return render_template('airwise.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
