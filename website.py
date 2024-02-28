e57700c28be8bcec8a76e6258d37878a22e8dded
Dx1au <149078244+Dx1au@users.noreply.github.com>
2024-02-28 18:48:17 +0000

    xnhf

--- website.py
+++ website.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.rout('/meow')
-def meow():
-    return 'MEOW.'
+    return render_template('airwise.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
