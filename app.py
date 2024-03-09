9d85c4dd0af21697173911c84589707f2dcc30f4
s12346938 <113122133+s12346938@users.noreply.github.com>
2024-03-09 19:29:44 +0000

    hostname change

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('airwise.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='AIRWISE.local', port=5000)
