fdc187f83c96b3f9cc2c659d55dba4c56bc16c88
s12346938 <113122133+s12346938@users.noreply.github.com>
2024-03-09 19:34:12 +0000

    template

--- templates/app.py
+++ templates/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('airwise.html')
+    return render_template('airwise.html', template_folder='templates')
 
 if __name__ == '__main__':
     app.run(debug=True, host='AIRWISE.local', port=5000)
